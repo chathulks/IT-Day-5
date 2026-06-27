6758b6fd1b2b1081e198eef1e0797e4cc2917895
chathulks <sadeeshachathumal5@gmail.com>
2026-06-27 16:02:40 +0530

    Refactor HTML form structure and add JavaScript functionality

---
+++ js/app.js
@@ -0,0 +1,133 @@
+// let f_name = "Sadeesha";
+// let age = 24;
+// let isAfterAL = true;
+// let customer = {};
+// let customers = [];
+// let salary = 1000000.0;
+
+// console.log(typeof f_name);
+// console.log(typeof age);
+// console.log(typeof isAfterAL);
+// console.log(typeof customer);
+// console.log(typeof customers);
+// console.log(typeof salary);
+
+// --------------------------------------------
+// + | - | * | /
+// let x = 10;
+// let y = 2;
+// console.log(x/y);
+// --------------------------------------------
+
+// < | > | <= | >= | != | ==
+// | <== | >== | !== | ===
+
+// let x = 10;
+// let y = "10";
+
+// console.log(x==y);
+
+// --------------------------------------------
+// && | || | !
+// --------------------------------------------
+
+// if(!false){
+//     console.log("true");
+
+// }else{
+//     console.log("false");
+// }
+
+// --------------------------------------------
+// let x = 10;
+// let y = 10;
+
+// if (x == y) {
+//     console.log("success");
+
+// } else {
+//     console.log("Error");
+// }
+// --------------------------------------------
+
+// --------------------------------------------
+// let x = 10;
+// let y = 5;
+
+// if (x == y) {
+//     console.log("success");
+
+// } else if (y > x) {
+//     console.log("Yes");
+// } else {
+//     console.log("Error");
+// }
+// --------------------------------------------
+
+// --------------------------------------------
+// let day = 1;
+
+// switch (day) {
+//     case 1:
+//         console.log("Sunday");
+//         break;
+//     case 2:
+//         console.log("Monday");
+//         break;
+//     case 3:
+//         console.log("Friday");
+//         break;
+//     case 4:
+//         console.log("Wensday");
+//         break;
+//     default:
+//         console.log("Error");
+//         break;
+// }
+// --------------------------------------------
+
+// --------------------------------------------
+// for (let index = 0; index < 20; index++) {
+//     console.log(index);
+// }
+// --------------------------------------------
+
+// --------------------------------------------
+// let x = 10;
+// while (x ) {
+
+// }
+// --------------------------------------------
+
+// --------------------------------------------
+// let i = 0;
+// do {
+//     console.log(i);
+//     i++;   
+// } while (i <= 10);
+// --------------------------------------------
+
+//---------------------------------------------
+// function add(x,y){
+//     return x+y;
+// }
+
+// let result = add(20,23);
+// console.log(result);
+//---------------------------------------------
+
+// let num = [1,"Sadeesha","Baduraliya",4,5,6,7];
+
+// for (let index = 0; index < num.length; index++) {
+//     console.log(num[index]);
+// }
+
+let customer = {
+    id: 1,
+    name: "Sadeesha",
+    age: 24,
+    mobile: "0704591135",
+    position: "Software Engineer"
+}
+
+console.log(customer);
